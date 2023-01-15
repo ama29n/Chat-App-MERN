68715a2f7cc23111a66a837e94e43ea253d3d4f5
ama29n <ama3149n@gmail.com>
2023-01-15 22:33:25 +0530

    get user credentials disabled

--- client/src/Components/SignupLogin/LoginForm.js
+++ client/src/Components/SignupLogin/LoginForm.js
@@ -68,7 +68,7 @@ function LoginForm() {
         {...{ type: showVisibility ? null : "password" }}
       />
       <PrimaryButton clickHandler={loginHandler} buttonText="Login" />
-      <Button variant="outlined" onClick={defualtUserCredentialsHandler}>
+      <Button disabled variant="outlined" onClick={defualtUserCredentialsHandler}>
         Get User Credential
       </Button>
     </Box>
